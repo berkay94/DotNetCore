aa6c069385db819943ed479b84b9df685eaacb79
semvs <semvs@ayvansaray.edu.tr>
2019-03-18 14:49:14 +0300

    Add project files.

---
+++ CoreWeb/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+       
+
+        public IActionResult Sayac(Iski k)
+        {
+
+            Fatura fat=getFatura(k);
+          
+            return View(fat);
+        }
+
+        public Fatura getFatura(Iski q)
+        {
+            Fatura fatura = new Fatura();
+            fatura.KullanilanMiktar = q.sonSayac - q.ilkSayac;
+          
+            fatura.AtikSuToplami = (decimal)(fatura.KullanilanMiktar * 0.3);
+            double birimfiyat = q.meskenTipi == "Umumi" ? 0.9 : 1.3;
+            fatura.AraToplam = (decimal)(fatura.KullanilanMiktar * birimfiyat);
+
+            return fatura;
+
+
+        }
+    }
+}
---
+++ CoreWeb/Models/Fatura.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWeb.Models
+{
+    public class Fatura
+    {
+        public int KullanilanMiktar { get; set; }
+
+        public int AtikSuMiktari { get; set; }
+
+        public decimal AraToplam { get; set; }
+
+        public decimal AtikSuToplami { get; set; }
+
+        public decimal GenelToplam {
+            get {
+                return AraToplam + AtikSuToplami;
+                }
+        }
+    }
+}
